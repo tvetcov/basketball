959276cb79ab014be381d9d4a93d81b9d504b61b
Nikita Tvetcov <nikita001809@gmail.com>
2019-06-07 17:37:21 +0300

    Added modal and videos to it

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,14 @@
         <div class="card">
             <div class="thumb-image">
                 <img v-bind:src="`${this.element.thumb}`" class="embed-responsive">
-                <span v-if="isVideo" class="play"></span>
+                <span v-if="isVideo">
+                    <span class="play" v-b-modal="showModal(this.element.id)"></span>
+                    <b-modal centered :id="'modal-' + this.element.id" :title="this.element.title">
+                         <video controls autoplay muted class="embed-responsive">
+                            <source :src="this.element.src" type="video/mp4">
+                        </video>
+                    </b-modal>
+                </span>
             </div>
             <div class="card-body d-flex justify-content-between">
                 <b class="text-uppercase m-0">{{this.element.title}}</b>
@@ -23,6 +30,11 @@
         computed: {
             isVideo: function () {
                 return this.element.type === 2;
+            },
+        },
+        methods: {
+            showModal(id) {
+                return 'modal-' + id;
             },
         },
         props: ['id']
